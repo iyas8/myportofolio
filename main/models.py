98c09b868cd201f7d196ecbff0695b84e60d371b
iyas8 <iyasderr@gmail.com>
2026-09-13 22:50:55 +0700

    add education model and migration

--- main/models.py
+++ main/models.py
@@ -24,3 +24,13 @@ class Experience(models.Model):
     @property
     def is_ongoing(self):
         return self.ended_at is None
+
+class Education(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    school = models.CharField(max_length=255)
+    degree = models.CharField(max_length=255)
+    start_year = models.CharField(max_length=4)
+    end_year = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.school
